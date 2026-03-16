2913355b2c61fad73b2c8c7c99ab11dd998e5146
Your Name <you@example.com>
2026-03-16 13:50:01 +0800

    cs and js file link to index.html

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Git Flow Project: Todo List is connected!");
-
-  const container = document.querySelector(".container");
-
-  if (container) {
-    console.log("this js is connected to index.html");
-  }
-});
